2513e77e6b291263ec0c606e6a8767bb999b0d39
Arjun Divraniya <arjun.divraniya.cg@email.com>
2024-11-07 13:55:04 +0530

    ferfar

---
+++ Untitled2.c
@@ -0,0 +1,26 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a=10;
+    float b=2.5;
+    float add=a+b;
+    float sub=a-b;
+    float  mul=a*b;
+    float divd=a/b;
+    printf("%1.1f\n",add);
+    printf("%1.1f\n",sub);
+    printf("%1.0f\n",mul);
+    printf("%1.1f\n",divd);
+
+<<<<<<< HEAD
+=======
+      printf("%1.1f\n",add);
+    printf("%1.1f\n",sub);
+    printf("%1.0f\n",mul);
+    printf("%1.1f\n",divd);
+    // Change by me
+
+>>>>>>> f748156 (change samthing)
+    return 0;
+}
